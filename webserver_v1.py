0ba33ed315ebc1c91f1e6a30579461e7c73b0cfe
Lief Koepsel <lkoepsel@wellys.com>
2023-05-05 07:55:43 -0700

    Initial commit

---
+++ webserver_v1.py
@@ -0,0 +1,36 @@
+# wlan_v2 - wireless lan connection, use LEDs for status
+from microdot import Microdot, send_file
+
+
+app = Microdot()
+
+
+@app.route('/')
+def index(request):
+    return send_file('./index.html')
+
+
+@app.post('/')
+def index_post(request):
+    level = request.form['level']
+    led = request.form['led']
+    print("Set", led, "led", level)
+    return send_file('./index.html')
+
+
+@app.route('bulma.min.css')
+def bulma(request):
+    return send_file('./bulma.min.css')
+
+
+@app.get('favicon.png')
+def favicon(request):
+    return send_file('./favicon.png', content_type='image/png')
+
+
+@app.get('computer.svg')
+def computer_svg(request):
+    return send_file('./computer.svg', content_type='image/svg+xml')
+
+
+app.run(debug=True)
