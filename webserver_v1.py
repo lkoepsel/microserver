3c1fb780dbf5010b9b879e8fdc8a76ba9b3c631c
Lief Koepsel <lkoepsel@wellys.com>
2023-05-07 14:41:33 -0700

    Reverted commit to 64f68fe2f5b

--- webserver_v1.py
+++ webserver_v1.py
@@ -1,20 +1,19 @@
 # webserver_v1 - simple tic-tac-toe game using micropython, microdot server
 # use on local machine
-from microdot import Microdot, Response, send_file, Request
+from microdot import Microdot, Response, send_file
 from microdot_utemplate import render_template
 
 
 app = Microdot()
 Response.default_content_type = 'text/html'
-Request.socket_read_timeout = None
 
 
 marks = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+disabled = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 pico_plays = [[0, 1], [2, 0], [2, 2],
               [1, 1], [0, 2], [1, 0],
               [1, 2], [0, 0], [2, 1]]
 pico = 0
-name = 'markO.tpl'
 
 
 def init():
@@ -22,22 +21,24 @@ def init():
     for i in range(3):
         for j in range(3):
             marks[i][j] = ' '
+            disabled[i][j] = ' '
     pico = 0
-    return marks
+    print(f"INIT{pico=}{marks=}{disabled=}")
+    return marks, disabled
 
 
 def pico_play(t):
     while(t):
         global pico
-
+        print(f"{pico=}")
+        print(f"{pico_plays[pico][0]=} {pico_plays[pico][1]=}")
         if marks[pico_plays[pico][0]][pico_plays[pico][1]] == 'X':
-            if pico >= 8:
-                # print(f"Pentultimate spot!{pico=}")
-                return True
+            print(pico_plays[pico][0], pico_plays[pico][1], " = X")
             pico += 1
 
         else:
             t = False
+            print(pico_plays[pico][0], pico_plays[pico][1], " = O")
             set_mark(pico_plays[pico][0], pico_plays[pico][1], 'O')
             pico += 1
 
@@ -59,71 +60,58 @@ def check_path(p):
         return True
     elif marks[0][1] == p and marks[1][1] == p and marks[2][1] == p:
         return True
-    elif marks[0][0] == p and marks[1][0] == p and marks[2][0] == p:
-        return True
     else:
         return False
 
 
 def set_mark(r, c, p):
     marks[r][c] = p
-    return marks
+    disabled[r][c] = 'disabled'
+    return marks, disabled
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index(request):
-    global pico
     turn = True
     square = None
     if request.method == 'POST':
         if 'square' in request.form.keys():
-            square = int(request.form['square'])
-            if square < 10:
-                row = 0
-                col = square
-            else:
-                col = square % 10
-                row = int((square - col) / 10)
-            print(f"{row=} {col=}")
+            square = request.form['square']
+            row = int(square[:1])
+            col = int(square[1:2])
+
             markup = set_mark(row, col, 'X')
-            draw = pico_play(turn)
-            if draw:
-                return send_file('./draw.html')
-            lost = check_path('O')
+            pico_play(turn)
+            won = check_path('O')
             won = check_path('X')
-            if lost:
-                return send_file('./lost.html')
-            elif won:
+            if won:
                 return send_file('./won.html')
             else:
-                return render_template('index.html')
+                return render_template('index.html',
+                                       marks=markup[0], disabled=markup[1])
         elif 'play' in request.form.keys():
             init()
             if request.form['play'] == 'Play Again?':
-                return render_template('index.html')
+                return render_template('index.html', marks=marks,
+                                       disabled=disabled)
     else:
-        # print(pico_plays)
-        return render_template('index.html')
+        print(pico_plays)
+        return render_template('index.html', marks=marks, disabled=disabled)
 
 
 @app.post('/won.html')
 def won_post(request):
-    return render_template('index.html')
+    return render_template('index.html', marks=marks, disabled=disabled)
 
 
 @app.post('/lost.html')
 def lost_post(request):
-    return render_template('index.html')
-
-
-@app.post('/draw.html')
-def draw_post(request):
-    return render_template('index.html')
+    return render_template('index.html', marks=marks, disabled=disabled)
 
 
 @app.route('bulma.min.css')
 def bulma(request):
-    return send_file('./bulma.min.css', max_age=31536000)
+    return send_file('./bulma.min.css')
 
 
 @app.get('favicon.png')
@@ -133,8 +121,7 @@ def favicon(request):
 
 @app.get('computer.svg')
 def computer_svg(request):
-    return send_file('./computer.svg', content_type='image/svg+xml',
-                     max_age=31536000)
+    return send_file('./computer.svg', content_type='image/svg+xml')
 
 
 app.run(debug=True)
