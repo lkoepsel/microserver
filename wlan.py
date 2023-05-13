35ec5e87da3f4b1dd597eb8ef8ac95765c78beb0
Lief Koepsel <lkoepsel@wellys.com>
2023-05-13 07:08:44 -0700

    updated wlan for config.py and STATUS errors

--- wlan.py
+++ wlan.py
@@ -2,10 +2,18 @@ import time
 import network
 from machine import Pin, Timer
 import secrets
+import config
 import ubinascii
 
 
 wireless = Pin("LED", Pin.OUT)
+STATUS = ['LINK_BADAUTH',
+          'LINK_NONET',
+          'LINK_FAIL',
+          'LINK_DOWN',
+          'LINK_JOIN',
+          'LINK_NOIP',
+          'LINK_UP']
 
 
 def tick(timer):
@@ -14,6 +22,7 @@ def tick(timer):
 
 
 def connect():
+    global STATUS
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(secrets.ssid, secrets.password)
@@ -33,14 +42,19 @@ def connect():
     # Handle connection error
     if wlan.status() != 3:
         blink.init(freq=20, mode=Timer.PERIODIC, callback=tick)
+        print(f"Connection failed: {STATUS[wlan.status()+3]}")
 
     else:
         blink.deinit()
         status = wlan.ifconfig()
         MAC = ubinascii.hexlify(network.WLAN().config('mac'), ':').decode()
-        print(f"Name: Pico W X")
+        print(f"Name: {config.name}")
         print(f"IP Address: {status[0]}")
         print(f"MAC Address: {MAC}")
         print(f"Channel: {wlan.config('channel')}")
         print(f"SSID: {wlan.config('essid')}")
         print(f"TX Power: {wlan.config('txpower')}")
+
+
+if __name__ == '__main__':
+    connect()
